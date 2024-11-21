689b42d2a7609dc0a59d4073c0dd29550e13c746
seungwonme <seungwonan.me@gmail.com>
2024-11-22 06:57:34 +0900

    chore: next.config.js 파일 수정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  ignoreDuringBuilds: true, // 빌드 시 ESLint 오류 무시
+  eslint: {
+    ignoreDuringBuilds: true, // 빌드 시 ESLint 오류 무시
+  },
 };
 
 module.exports = nextConfig;
